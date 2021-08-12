2725a333a79575add967cab72f0da584b93260e6
Jayavardhan Tummidi <jayavardhantummidi@Jayavardhans-MacBook-Pro.local>
2021-08-11 20:26:11 -0400

    s3

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
   secret_key = var.aws_secret_access_key
 }
 
-resource "aws_s3_bucket" "this" {
+resource "aws_s3_bucket" "jayaworld_s3" {
   bucket = var.bucket_name
   acl    = "private"
 }
--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "id" {
   description = "The name of the bucket"
-  value       = aws_s3_bucket.this.id
+  value       = aws_s3_bucket.jayaworld_s3.id
 }
 
 output "arn" {
   description = "The ARN of the bucket. Will be of format arn:aws:s3:::bucketname."
-  value       = aws_s3_bucket.this.arn
+  value       = aws_s3_bucket.jayaworld_s3.arn
 }
