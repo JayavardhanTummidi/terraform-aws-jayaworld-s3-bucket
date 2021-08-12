a42910ac50f8be35e1d1c32b649f64758348ddd6
Jayavardhan Tummidi <jayavardhantummidi@Jayavardhans-MacBook-Pro.local>
2021-08-12 18:21:47 -0400

    s3 bucket with KMS Key

--- main.tf
+++ main.tf
@@ -1,9 +1,34 @@
 provider "aws" {
-  region = "${var.region}"
+  region = var.region
 }
 
 resource "aws_s3_bucket" "jaya-world-s3" {
   bucket = var.bucket_name
-  acl    = "private"
-  tags = merge(var.tags)
+  acl    = "private" # available are public, public-read
+  tags   = merge(var.tags)
+  # enable version control on objects
+  versioning = {
+    enabled = true
+  }
+  # To encrypt the data, creating KMS key 
+  resource "aws_kms_key" "jaya-world-kms-s3" {
+    description              = "kms key for s3"
+    key_usage                = "ENCRYPT_DECRYPT"
+    customer_master_key_spec = "SYMMETRIC_DEFAULT"
+    tags = {
+      resource = "s3"
+    }
+  }
+  # Passing the KMS key to encrypt the data 
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_defualt {
+        kms_master_key_id = aws_kms_key.jaya-world-kms-s3.arn
+        sse_algorithm     = "aws::kms"
+        tags              = merge(var.tags)
+      }
+    }
+
+  }
+
 }
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,4 @@
+# output of S3 bucket
 output "id" {
   description = "The name of the bucket"
   value       = aws_s3_bucket.jaya-world-s3.id
@@ -6,4 +7,14 @@ output "id" {
 output "arn" {
   description = "The ARN of the bucket. Will be of format arn:aws:s3:::bucketname."
   value       = aws_s3_bucket.jaya-world-s3.arn
+}
+# output of KMS key for S3 bucket data encryption
+output "key_id" {
+  description = "KMS Key ID"
+  value       = aws_kms_key.jaya-world-kms-s3.key_id
+}
+
+output "arn" {
+  description = "KMS Key ARN"
+  value       = aws_kms_key.jaya-world-kms-s3.arn
 }
