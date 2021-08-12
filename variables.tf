1e849fb12ede84442472ddb49c93827ba370b244
Jayavardhan Tummidi <jayavardhantummidi@Jayavardhans-MacBook-Pro.local>
2021-08-11 20:21:02 -0400

    s3 publish module

--- variables.tf
+++ variables.tf
@@ -14,3 +14,12 @@ variable "tags" {
   type        = map(string)
   default     = {}
 }
+
+variable "aws_access_key_id" {
+  type    = string
+  default = "AKIAVGLA7FRWMSC4D7GD"
+}
+variable "aws_secret_access_key" {
+  type    = string
+  default = "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"
+} 
