66ddfefc5f76d4976759d7c98a5ddaca49a36df7
Jayavardhan Tummidi <jayavardhantummidi@Jayavardhans-MacBook-Pro.local>
2021-08-11 22:29:31 -0400

    s3

--- main.tf
+++ main.tf
@@ -2,7 +2,8 @@ provider "aws" {
   region = "${var.region}"
 }
 
-resource "aws_s3_bucket" "jayaworld_s3" {
+resource "aws_s3_bucket" "jaya-world-s3" {
   bucket = var.bucket_name
   acl    = "private"
+  tags = merge(var.tags)
 }
--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "id" {
   description = "The name of the bucket"
-  value       = aws_s3_bucket.jayaworld_s3.id
+  value       = aws_s3_bucket.jaya-world-s3.id
 }
 
 output "arn" {
   description = "The ARN of the bucket. Will be of format arn:aws:s3:::bucketname."
-  value       = aws_s3_bucket.jayaworld_s3.arn
+  value       = aws_s3_bucket.jaya-world-s3.arn
 }
