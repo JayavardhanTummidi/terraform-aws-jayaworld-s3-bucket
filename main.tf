53ebb8a395e03a6e367b849aafc32f3b2ce2b484
Jayavardhan Tummidi <jayavardhantummidi@Jayavardhans-MacBook-Pro.local>
2021-08-12 23:47:50 -0400

    s3

--- main.tf
+++ main.tf
@@ -2,9 +2,9 @@ provider "aws" {
   region = var.region
 }
 # for managing custom managed KMS key
-data "aws_kms_key" "this" {
-  key_id = var.aws_kms_key_arn
-}
+#data "aws_kms_key" "this" {
+# key_id = var.aws_kms_key_arn
+#} 
 # New resource for KMS Key creation
 resource "aws_kms_key" "jaya-world-kms-key" {
   description = "for s3 buckets encryption"
@@ -24,7 +24,8 @@ resource "aws_s3_bucket" "jaya-world-s3" {
     rule {
       apply_server_side_encryption_by_default {
         #kms_master_key_id = aws_kms_key.jaya-world-kms-key.arn
-        kms_master_key_id = data.aws_kms_key.this.id == null ? "KMS key is not valid" : var.aws_kms_key_arn
+        #kms_master_key_id = data.aws_kms_key.this.id == null ? "KMS key is not valid" : var.aws_kms_key_arn
+         kms_master_key_id = var.aws_kms_key_arn
         sse_algorithm = "aws:kms"
       }
     }
