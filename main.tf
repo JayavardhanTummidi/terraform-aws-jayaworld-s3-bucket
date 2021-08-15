a8e68b655229b8e564f55d8457f84c49d8dc10e2
Jayavardhan Tummidi <jayavardhantummidi@Jayavardhans-MacBook-Pro.local>
2021-08-15 11:50:53 -0400

    s3

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 # Create a new bucket for logging and refer this resource in the original bucket creation to log the files. 
 resource "aws_s3_bucket" "log_bucket" {
   bucket = "jaya-world-log-bucket-hello"
-  acl = "log-delivery-write"
+  #acl = "log-delivery-write"
 }
 # Manage public access settings for the S3 log bucket. 
 resource "aws_s3_bucket_public_access_block" "log_bucket" {
@@ -43,10 +43,11 @@ resource "aws_s3_bucket" "jaya-world-s3" {
     }
   }
   # enable logging 
+  /*
   logging {
     target_bucket = aws_s3_bucket.log_bucket.id
     target_prefix = "log/"
-  }
+  } */
 }
 
 
