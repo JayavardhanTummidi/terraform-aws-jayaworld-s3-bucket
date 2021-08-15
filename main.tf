9e055c4474ba2f6fa1d46398fcef95ac22b30015
Jayavardhan Tummidi <jayavardhantummidi@Jayavardhans-MacBook-Pro.local>
2021-08-15 11:26:13 -0400

    s3

--- main.tf
+++ main.tf
@@ -2,6 +2,24 @@ provider "aws" {
   region = var.region
 }
 
+# Manage public access settings for the S3 log bucket. 
+resource "aws_s3_bucket_public_access_block" "public_access_block" {
+  bucket = aws_s3_bucket.log_bucket.id
+
+  # S3 will block public access permissions applied to newly added buckets or objects, and prevent the creation of new public access ACLs for existing buckets and objects. This setting doesn’t change any existing permissions that allow public access to S3 resources using ACLs.
+  block_public_acls = false
+
+  # S3 will ignore all ACLs that grant public access to buckets and objects.
+  ignore_public_acls = false
+  
+  # S3 will block new bucket and access point policies that grant public access to buckets and objects. This setting doesn't change any existing policies that allow public access to S3 resources
+  block_public_policy = false
+
+  # S3 will ignore public and cross-account access for buckets or access points with policies that grant public access to buckets and objects.
+  restrict_public_buckets = false
+
+}
+
 # Create a new bucket for logging and refer this resource in the original bucket creation to log the files. 
 resource "aws_s3_bucket" "log_bucket" {
   bucket = "jaya-world-log-bucket-hello"
@@ -30,23 +48,6 @@ resource "aws_s3_bucket" "jaya-world-s3" {
     target_bucket = aws_s3_bucket.log_bucket.id
     target_prefix = "log/"
   }
-}
-# Manage public access settings for the S3 log bucket. 
-resource "aws_s3_bucket_public_access_block" "public_access_block" {
-  bucket = aws_s3_bucket.log_bucket.id
-
-  # S3 will block public access permissions applied to newly added buckets or objects, and prevent the creation of new public access ACLs for existing buckets and objects. This setting doesn’t change any existing permissions that allow public access to S3 resources using ACLs.
-  block_public_acls = false
-
-  # S3 will ignore all ACLs that grant public access to buckets and objects.
-  ignore_public_acls = false
-  
-  # S3 will block new bucket and access point policies that grant public access to buckets and objects. This setting doesn't change any existing policies that allow public access to S3 resources
-  block_public_policy = false
-
-  # S3 will ignore public and cross-account access for buckets or access points with policies that grant public access to buckets and objects.
-  restrict_public_buckets = false
-
 }
 
 
