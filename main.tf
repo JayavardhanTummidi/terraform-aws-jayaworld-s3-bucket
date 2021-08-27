64238cbfe842f3b577825eb10753938bbc47b640
Jayavardhan Tummidi <jayavardhantummidi@Jayavardhans-MacBook-Pro.local>
2021-08-26 20:23:35 -0400

    s3

--- main.tf
+++ main.tf
@@ -3,21 +3,21 @@ provider "aws" {
 }
 # Create a new bucket server logging
 resource "aws_s3_bucket" "jaya_world_log_bucket" {
-   bucket = var.log_bucket_name
-   acl = "log-delivery-write"
-   policy = var.log_bucket_policy
-   force_destroy = var.log_bucket_force_destroy
-   tags = merge(var.tags)
-   # enable version control on objects
-   versioning  {
+  bucket        = var.log_bucket_name
+  acl           = "log-delivery-write"
+  policy        = var.log_bucket_policy
+  force_destroy = var.log_bucket_force_destroy
+  tags          = merge(var.tags)
+  # enable version control on objects
+  versioning {
     enabled = true
   }
   # making server side encryption by default
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-         kms_master_key_id = var.aws_kms_key_arn
-         sse_algorithm = "aws:kms"
+        kms_master_key_id = var.aws_kms_key_arn
+        sse_algorithm     = "aws:kms"
       }
       bucket_key_enabled = "true"
     }
@@ -25,21 +25,21 @@ resource "aws_s3_bucket" "jaya_world_log_bucket" {
 
   # Lifecyle rule for logs
   lifecycle_rule {
-    id = "log"
-    prefix = "log/"
+    id      = "log"
+    prefix  = "log/"
     enabled = true
     tags = {
-     rule = "log"
-     autoclean = "true"
-    } 
+      rule      = "log"
+      autoclean = "true"
+    }
 
     transition {
-      days = 30
+      days          = 30
       storage_class = "STANDARD_IA"
     }
 
     transition {
-      days = 60
+      days          = 60
       storage_class = "GLACIER"
     }
 
@@ -51,35 +51,36 @@ resource "aws_s3_bucket" "jaya_world_log_bucket" {
 }
 # Create a new bucket
 resource "aws_s3_bucket" "jaya-world-s3" {
-  bucket = var.bucket_name
-  acl    = var.acl
-  policy = var.policy
-  force_destroy = var.force_destroy
-  request_payer = var.request_payer
-  tags   = merge(var.tags)
+  bucket              = var.bucket_name
+  acl                 = var.acl
+  policy              = var.policy
+  force_destroy       = var.force_destroy
+  request_payer       = var.request_payer
+  acceleration_status = var.acceleration_status
+  tags                = merge(var.tags)
   # enable version control on objects
-  versioning  {
+  versioning {
     enabled = true
   }
   # making server side encryption by default
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-         kms_master_key_id = var.aws_kms_key_arn
-         sse_algorithm = "aws:kms"
+        kms_master_key_id = var.aws_kms_key_arn
+        sse_algorithm     = "aws:kms"
       }
       bucket_key_enabled = "true"
     }
   }
-  
+
   # static website hosting
   dynamic "website" {
     for_each = var.website_rules
 
     content {
-    index_document = website.value.index_document
-    error_document = website.value.error_document
-    redirect_all_requests_to = website.value.redirect_all_requests_to
+      index_document           = website.value.index_document
+      error_document           = lookup(website.value, "error_document", null)
+      redirect_all_requests_to = lookup(website.value, "redirect_all_requests_to", null)
     }
   }
 
@@ -91,8 +92,8 @@ resource "aws_s3_bucket" "jaya-world-s3" {
 
   # Dynamic Lifecycle rule for incomplete multipart upload objects and expired object delete markers.
   lifecycle_rule {
-    id = "Abort_multipart_and_expired_object_delete_marker"
-    enabled = "true"
+    id                                     = "Abort_multipart_and_expired_object_delete_marker"
+    enabled                                = "true"
     abort_incomplete_multipart_upload_days = var.abort_incomplete_multipart_upload_days
     expiration {
       expired_object_delete_marker = "true"
@@ -101,26 +102,26 @@ resource "aws_s3_bucket" "jaya-world-s3" {
 
   # Dynamic lifecycle rule for noncurrent version transition. previous version. 
   lifecycle_rule {
-    id = "Rule_for_previous_versions"
+    id      = "Rule_for_previous_versions"
     enabled = "true"
 
     dynamic "noncurrent_version_transition" {
       for_each = var.noncurrent_version_transitions
 
       content {
-        days = noncurrent_version_transition.value.days
+        days          = noncurrent_version_transition.value.days
         storage_class = noncurrent_version_transition.value.storage_class
       }
     }
     noncurrent_version_expiration {
-       days = var.previous_version_expiration_days
+      days = var.previous_version_expiration_days
     }
-       
+
   }
   # Dynamic lifecycle rule for current versions with the filter option
   lifecycle_rule {
-    id = var.lifecycle_rule_id
-    prefix = var.lifecycle_rule_prefix
+    id      = var.lifecycle_rule_id
+    prefix  = var.lifecycle_rule_prefix
     enabled = "true"
     expiration {
       days = var.expiration_days
@@ -130,13 +131,85 @@ resource "aws_s3_bucket" "jaya-world-s3" {
       for_each = var.transitions
 
       content {
-        days = transition.value.days
+        days          = transition.value.days
         storage_class = transition.value.storage_class
       }
     }
 
   }
+  # For replication configuration
+
+  dynamic "replication_configuration" {
+    for_each = var.replication_configuration
+
+    content {
+      role = replication_configuration.value.role
+
+      dynamic "rules" {
+        for_each = replication_configuration.value.rules
+
+        content {
+          delete_marker_replication_status = lookup(rules.value, "delete_marker_replication_status", null)
+          priority                         = lookup(rules.value, "priority", null)
+          status                           = rules.value.status
+          id                               = lookup(rules.value, "id", null)
+          prefix                           = lookup(rules.value, "prefix", null)
+
+          dynamic "destination" {
+            for_each = lookup(rules.value, "destination", null)
+
+            content {
+              bucket             = destination.value.bucket
+              storage_class      = lookup(destination.value, "storage_class", null)
+              replica_kms_key_id = lookup(destination.value, "replica_kms_key_id", null)
+              account_id         = lookup(destination.value, "account_id", null)
+
+              dynamic "access_control_translation" {
+                for_each = lookup(destination.value, "access_control_translation", null)
+
+                content {
+                  owner = access_control_translation.value.owner
+                }
+
+              }
+            }
+          }
+
+          dynamic "filter" {
+            for_each = lookup(rules.value, "filter", null)
+
+            content {
+              prefix = lookup(filter.value, "prefix", null)
+              tags   = lookup(filter.value, "tags", null)
+            }
+          }
+
+          dynamic "source_selection_criteria" {
+            for_each = lookup(rules.value, "source_selection_criteria", null)
+
+            content {
+
+              dynamic "sse_kms_encrypted_objects" {
+                for_each = lookup(source_selection_criteria.value, "sse_kms_encrypted_objects", null)
+
+                content {
+
+                  enabled = lookup(sse_kms_encrypted_objects.value, "enabled", null)
+
+                }
+
+              }
+
+            }
+          }
+        }
+      }
+    }
+  }
 }
+
+
+
 
 # Manage public access settings for the S3 log bucket. 
 resource "aws_s3_bucket_public_access_block" "jaya-world-s3" {
@@ -147,7 +220,7 @@ resource "aws_s3_bucket_public_access_block" "jaya-world-s3" {
 
   # S3 will ignore all ACLs that grant public access to buckets and objects.
   ignore_public_acls = var.ignore_public_acls
-  
+
   # S3 will block new bucket and access point policies that grant public access to buckets and objects. This setting doesn't change any existing policies that allow public access to S3 resources
   block_public_policy = var.block_public_policy
 
@@ -163,7 +236,7 @@ resource "aws_s3_bucket_public_access_block" "jaya_world_log_bucket" {
 
   # S3 will ignore all ACLs that grant public access to buckets and objects.
   ignore_public_acls = true
-  
+
   # S3 will block new bucket and access point policies that grant public access to buckets and objects. This setting doesn't change any existing policies that allow public access to S3 resources
   block_public_policy = true
 
--- variables.tf
+++ variables.tf
@@ -7,8 +7,8 @@ variable "log_bucket_name" {
   type        = string
 }
 variable "aws_kms_key_arn" {
-   description = "The customer managed(CMK) to encrypt the bucket. Please create KMS Key if you haven't done already"
-   type = string
+  description = "The customer managed(CMK) to encrypt the bucket. Please create KMS Key if you haven't done already"
+  type        = string
 }
 variable "bucket_name" {
   description = "please provide bucket name"
@@ -17,81 +17,81 @@ variable "bucket_name" {
 
 variable "acl" {
   description = "The canned ACL to apply. Valid values are private, public-read, public-read-write, aws-exec-read, authenticated-read, and log-delivery-write. Defaults to private. Conflicts with grant"
-  type = string
-  default = "private"
+  type        = string
+  default     = "private"
 }
 
 variable "policy" {
   description = "A valid bucket policy JSON document. Note that if the policy document is not specific enough (but still valid), Terraform may view the policy as constantly changing in a terraform plan"
-  type = string
-  default = ""
+  type        = string
+  default     = ""
 }
 
 variable "log_bucket_policy" {
   description = "A valid bucket policy JSON document. Note that if the policy document is not specific enough (but still valid), Terraform may view the policy as constantly changing in a terraform plan"
-  type = string
-  default = ""
+  type        = string
+  default     = ""
 }
 
 variable "force_destroy" {
   description = "Whether to allow the object to be deleted by removing any legal hold on any object version. Default is false. This value should be set to true only if the bucket has S3 object lock enabled."
-  type = bool
-  default = "false"
+  type        = bool
+  default     = "false"
 }
 
 variable "log_bucket_force_destroy" {
   description = "Whether to allow the object to be deleted by removing any legal hold on any object version. Default is false. This value should be set to true only if the bucket has S3 object lock enabled."
-  type = bool
-  default = "false" 
+  type        = bool
+  default     = "false"
 }
 
 variable "acceleration_status" {
   description = "Transfer Acceleration takes advantage of the globally distributed edge locations in Amazon CloudFront. As the data arrives at an edge location, the data is routed to Amazon S3 over an optimized network path."
-  type = string
-  default = "Suspended"
+  type        = string
+  default     = null
 }
 
 variable "request_payer" {
   description = "Specifies who should bear the cost of Amazon S3 data transfer. Can be either BucketOwner or Requester. By default, the owner of the S3 bucket would incur the costs of any data transfer. "
-  type = string
-  default = "BucketOwner"
+  type        = string
+  default     = "BucketOwner"
 }
 
 variable "abort_incomplete_multipart_upload_days" {
   description = "Specifies the number of days after initiating a multipart upload when the multipart upload must be completed."
-  type = number
-  default = 7
+  type        = number
+  default     = 7
 }
 
 variable "expiration_days" {
   description = "Specifies the number of days after object creation when the specific rule action takes effect. This is for current version"
-  type = number
-  default = "365"
+  type        = number
+  default     = "365"
 }
 
 variable "previous_version_expiration_days" {
   description = "Specifies the number of days after object becomes non-current"
-  type = number
-  default = "90"
+  type        = number
+  default     = "90"
 }
 
 variable "noncurrent_version_transitions" {
   description = "Specifies the number of days noncurrent object versions transition and Specifies the Amazon S3 storage class to which you want the noncurrent object versions to transition. Can be ONEZONE_IA, STANDARD_IA, INTELLIGENT_TIERING, GLACIER, or DEEP_ARCHIVE."
-  type = list
+  type        = list(any)
   default = [
-    { 
-     days= "30"
-     storage_class = "STANDARD_IA"
-   }
+    {
+      days          = "30"
+      storage_class = "STANDARD_IA"
+    }
   ]
 }
 
 variable "transitions" {
   description = "Specifies the number of days after object creation when the specific rule action takes effect and Specifies the Amazon S3 storage class to which you want the object to transition. Can be ONEZONE_IA, STANDARD_IA, INTELLIGENT_TIERING, GLACIER, or DEEP_ARCHIVE."
-  type = list
+  type        = list(any)
   default = [
     {
-      days = "365"
+      days          = "364"
       storage_class = "STANDARD_IA"
     }
   ]
@@ -99,50 +99,50 @@ variable "transitions" {
 
 variable "lifecycle_rule_id" {
   description = "Unique identifier for the lifecycle rule. Must be less than or equal to 255 characters in length."
-  type = string
-  default = "Rule_for_current_versions"
+  type        = string
+  default     = "Rule_for_current_versions"
 }
 
 variable "lifecycle_rule_prefix" {
   description = "Object key prefix identifying one or more objects to which the rule applies."
-  type = string
-  default = ""
+  type        = string
+  default     = ""
 }
 
 variable "website_rules" {
   description = "provide following optional inputs - 'index_document', 'error_document' and 'redirect_all_requests_to' to enable statis website"
-  type = list 
-  default = [
-    {
-      index_document = "index.html"
-      error_document = "error.html"
-      redirect_all_requests_to = null
-    }
-  ]
+  type        = map(any)
+  default     = {}
 }
 
 variable "block_public_acls" {
   description = "S3 will block public access permissions applied to newly added buckets or objects, and prevent the creation of new public access ACLs for existing buckets and objects. This setting doesn’t change any existing permissions that allow public access to S3 resources using ACLs"
-  type = string
-  default = "true"
+  type        = string
+  default     = "true"
 }
 
 variable "ignore_public_acls" {
   description = "S3 will ignore all ACLs that grant public access to buckets and objects."
-  type = string
-  default = "true"
+  type        = string
+  default     = "true"
 }
 
 variable "block_public_policy" {
   description = "S3 will block new bucket and access point policies that grant public access to buckets and objects. This setting doesn't change any existing policies that allow public access to S3 resources"
-  type = string
-  default = "true"
+  type        = string
+  default     = "true"
 }
 
 variable "restrict_public_buckets" {
   description = "S3 will ignore public and cross-account access for buckets or access points with policies that grant public access to buckets and objects."
-  type = string
-  default = "true"
+  type        = string
+  default     = "true"
+}
+
+variable "replication_configuration" {
+  description = "Specifies the replication configuration"
+  type        = map(any)
+  default     = {}
 }
 
 variable "tags" {
